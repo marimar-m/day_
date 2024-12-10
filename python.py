a6b3a3cb14b277f81a3c4b9aeb93f6b32aaa26f6
marimar-m <marianamachado481@gmail.com>
2024-12-10 14:41:33 -0300

    Add files via upload

---
+++ python.py
@@ -0,0 +1,23 @@
+def print_heart():
+    heart = [
+        "      day day day       day day day        ",
+        "    day day day day  day day day day day    ",
+        "  day day day day day day day day day day   ",
+        " day day day day day day day day day day   ",
+        " day day day day day day day day day day   ",
+        "  day day day day day day day day day    ",
+        "    day day day day day day day day     ",
+        "      day day day day day day day       ",
+        "        day day day day day day         ",
+        "          day day day day day           ",
+        "            day day day day             ",
+        "              day day day               ",
+        "                  day                     "
+    ]
+    
+    # Imprime o coração
+    for line in heart:
+        print(line)
+
+# Chama a função para exibir o coração
+print_heart()
